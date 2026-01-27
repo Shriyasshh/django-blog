7cc2c764156b61a822a6a01bea23ce83069e1fcd
Shriyash Dharkar <shriyashdharkar@gmail.com>
2026-01-27 22:10:43 +0530

    Created Blog App, and models

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'blog_main/static',]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
