5986b4c6d22157a8cabe81e55dfab79d2cce8900
Shriyash Dharkar <shriyashdharkar@gmail.com>
2026-01-29 18:48:16 +0530

    Created Blog viewing page , Build dynamic About, Social link ,and displaying , searching blog

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from blogs.models import Category,Blog
+from assignment.models import About
 
 def  home(request):
     featured = Blog.objects.filter(is_featured=True, status = 'published').order_by('-created_at')
     posts = Blog.objects.filter(is_featured=False , status = 'published').order_by('-created_at')
-
+    try:
+        about = About.objects.get()
+    except:
+        about = None
 
     context = {
        'featured':featured,
-       'posts':posts
+       'posts':posts,
+       'about':about
                }
-    return render(request, "home.html",context)
+    return render(request , 'home.html', context)
--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,4 +1,10 @@
 from . models import Category
+from assignment.models import SocialLinks
+
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+def get_social_Links(request):
+    social_links = SocialLinks.objects.all()
+    return dict(social_links=social_links)
